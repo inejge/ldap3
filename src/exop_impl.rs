e2542e0e1408aa374a3c3fff92384300f443729e
Sangwan Jeon <commits@swjeon.kr>
2024-04-26 00:55:05 +0900

    feat: add deserialize for exop

--- src/exop_impl.rs
+++ src/exop_impl.rs
@@ -2,7 +2,7 @@ use lber::common::TagClass;
 use lber::structures::{OctetString, Tag};
 
 #[cfg(feature = "serde")]
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 mod whoami;
 pub use self::whoami::{WhoAmI, WhoAmIResp};
@@ -37,6 +37,58 @@ impl Serialize for Exop {
         seq.serialize_field("name", &self.name)?;
         seq.serialize_field("val", &self.val)?;
         seq.end()
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'de> Deserialize<'de> for Exop {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        use serde::de::Error;
+        use serde::de::MapAccess;
+        use serde::de::Visitor;
+        use std::fmt;
+
+        struct ExopVisitor;
+        impl<'de> Visitor<'de> for ExopVisitor {
+            type Value = Exop;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a struct with `name` and `val` fields")
+            }
+
+            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+            where
+                A: MapAccess<'de>,
+            {
+                let mut name = None;
+                let mut val = None;
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        "name" => {
+                            if name.is_some() {
+                                return Err(A::Error::duplicate_field("name"));
+                            }
+                            name = Some(map.next_value()?);
+                        }
+                        "val" => {
+                            if val.is_some() {
+                                return Err(A::Error::duplicate_field("val"));
+                            }
+                            val = Some(map.next_value()?);
+                        }
+                        _ => {
+                            return Err(A::Error::unknown_field(key, &["name", "val"]));
+                        }
+                    }
+                }
+                Ok(Exop { name, val })
+            }
+        }
+
+        deserializer.deserialize_struct("Exop", &["name", "val"], ExopVisitor)
     }
 }
 
